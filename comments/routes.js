f0508f766b33927f8a88ff308ae5f8aa81ac2380
AidaRos95 <aidajros@gmail.com>
2019-06-13 15:54:09 +0200

    Added route handler for posting comments

--- comments/routes.js
+++ comments/routes.js
@@ -14,10 +14,18 @@ router.get('/comments', (req, res) => {
 })
 
 router.post('/comments', (req, res) => {
-	res.json({
-		message: 'Post request to comments received',
-		request: req.body
-	})
+	console.log('REQUEST RECEIVED:', req.body)
+	Comment.create({ content: req.body.comment, ticketId: req.body.ticketId })
+		.then(comment => {
+			res.status(201).json({ 
+				message: 'comment created',
+				comment: comment })
+		})
+	
+	// res.json({
+	// 	message: 'Post request to comments received',
+	// 	request: req.body
+	// })
 })
 
 module.exports = router;
