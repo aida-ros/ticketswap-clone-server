642d49e18a6b60d8772b0291841610eab0d7475f
AidaRos95 <aidajros@gmail.com>
2019-06-13 12:57:09 +0200

    Problems with POST request users

--- users/routes.js
+++ users/routes.js
@@ -4,28 +4,28 @@ const User = require("./model");
 
 const router = express.Router()
 
-router.get('/users', (req, res) => {
-  User.findAll()
-		.then(users => {
-			console.log(users)
-			res.status(200).json({ users })
-		})
-		.catch(console.error());
-})
+// router.get('/users', (req, res) => {
+//   User.findAll()
+// 		.then(users => {
+// 			console.log(users)
+// 			res.status(200).json({ users })
+// 		})
+// 		.catch(console.error());
+// })
 
 // Creates a new user
-router.post('/users', (req, res) => {
-	// res.json({
-	// 	message: 'Post request to users received',
-	// 	request: req.body
-	// })
-	User.create(req.body)
-		.then(event => {
-			res.status(201).json({ 
-				message: 'User created',
-				event: event })
-		})
-})
+// router.post('/users', (req, res) => {
+// 	// res.json({
+// 	// 	message: 'Post request to users received',
+// 	// 	request: req.body
+// 	// })
+// 	User.create(req.body)
+// 		.then(user => {
+// 			res.status(201).json({ 
+// 				message: 'User created',
+// 				user: user })
+// 		})
+// })
 
 // User.create({ username: 'FirstUser', password: 'something' })
 // User.create({ username: 'SecondUser', password: 'test' })
