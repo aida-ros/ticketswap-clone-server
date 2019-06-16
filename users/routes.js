d6a93b785efe85f8a94891b17fb9b5fa47b59ffa
AidaRos95 <aidajros@gmail.com>
2019-06-16 14:02:43 +0200

    User data is added to the database during sign up

--- users/routes.js
+++ users/routes.js
@@ -4,31 +4,30 @@ const User = require("./model");
 
 const router = express.Router()
 
-// router.get('/users', (req, res) => {
-//   User.findAll()
-// 		.then(users => {
-// 			console.log(users)
-// 			res.status(200).json({ users })
-// 		})
-// 		.catch(console.error());
-// })
+router.get('/users', (req, res) => {
+  User.findAll()
+		.then(users => {
+			console.log(users)
+			res.status(200).json({ users })
+		})
+		.catch(console.error());
+})
 
 // Creates a new user
-// router.post('/users', (req, res) => {
-// 	// res.json({
-// 	// 	message: 'Post request to users received',
-// 	// 	request: req.body
-// 	// })
-// 	User.create(req.body)
-// 		.then(user => {
-// 			res.status(201).json({ 
-// 				message: 'User created',
-// 				user: user })
-// 		})
-// })
-
-// User.create({ username: 'FirstUser', password: 'something' })
-// User.create({ username: 'SecondUser', password: 'test' })
-// User.create({ username: 'ThirdUser', password: 'secret' })
+router.post('/users', (req, res) => {
+	// res.json({
+	// 	message: 'Post request to users received',
+	// 	request: req.body
+	// })
+	User.create({
+    username: req.body.username,
+    password: req.body.password
+  })
+		.then(user => {
+			res.status(201).json({ 
+				message: 'User created',
+				user: user })
+		})
+})
 
 module.exports = router;
